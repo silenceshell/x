44cfd33ebaa319c23bb6229ebe2df3747d82574e
silenceshell <me@ieevee.com>
2017-12-14 19:27:00 +0800

    v0.1

--- where-am-i.go
+++ where-am-i.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/yinheli/qqwry"
+	"html/template"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	flag "github.com/spf13/pflag"
 )
 
 var q *qqwry.QQwry
+var t *template.Template
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8888, "specify server port, default 8888")
+
 	q = qqwry.NewQQwry("qqwry.dat")
+	t, _ = template.ParseFiles("tmpl/index.html")
 
 	http.HandleFunc("/", indexHandler)
-	err := http.ListenAndServe(":88", nil)
+	var address string = fmt.Sprintf(":%d", port)
+	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal("server start failed.", err)
 	}
@@ -28,7 +37,39 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		q.Find(ip)
-		io.WriteString(w, "您好，来自"+q.Country+q.City+"的朋友！您的ip地址是："+ip)
+		err = t.Execute(w, map[string]string{"Country": q.Country, "City": q.City, "Ip": ip})
+		if err != nil {
+			io.WriteString(w, "internal error")
+			return
+		}
+	} else if r.Method == "POST" {
+		r.ParseForm()
+
+		ipx := r.Form["ipx"][0]
+		var message string
+		if net.ParseIP(ipx) == nil {
+			message = ipx + "格式不合法。"
+		} else {
+			q.Find(ipx)
+			message = ipx + "的地址是： " + q.Country + q.City
+		}
+
+		country, city, err := getCountryAndCity(r.RemoteAddr)
+		if err != nil {
+			io.WriteString(w, "internal error")
+			return
+		}
+
+		t.Execute(w, map[string]string{"Country": country, "City": city, "Ip": r.RemoteAddr, "Message": message})
 	}
 	return
+}
+
+func getCountryAndCity(ip string) (country, city string, err error) {
+	ip, _, e := net.SplitHostPort(ip)
+	if e != nil {
+		return "", "", fmt.Errorf("internal error")
+	}
+	q.Find(ip)
+	return q.City, q.City, nil
 }
