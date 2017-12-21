46f811c637a854ee7859d57bcb9c2794dfee5aa1
silenceshell <me@ieevee.com>
2017-12-21 17:55:35 +0800

    update

--- resource/database.sql
+++ resource/database.sql
@@ -7,9 +7,11 @@ CREATE TABLE visitor (ip varchar(64) NOT NULL, create_time datetime NOT NULL);
 
 DROP TABLE if EXISTS `shorturl`;
 CREATE TABLE `shorturl` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `long` varchar(255) NOT NULL,
-  `short` varchar(255) DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `long_url` varchar(255) NOT NULL,
+  `short_url` varchar(255) DEFAULT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO shorturl ( long_url, short_url, create_time ) VALUES ("xxxxx", "x", "xxxx");
