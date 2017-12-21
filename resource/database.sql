bd498bdfd6a1086d0b0bb7e224b5a5b11c2c64ce
silenceshell <me@ieevee.com>
2017-12-21 17:31:39 +0800

    update

--- resource/database.sql
+++ resource/database.sql
@@ -4,3 +4,12 @@ use x;
 
 DROP TABLE if EXISTS `visitor`;
 CREATE TABLE visitor (ip varchar(64) NOT NULL, create_time datetime NOT NULL);
+
+DROP TABLE if EXISTS `shorturl`;
+CREATE TABLE `shorturl` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `long` varchar(255) NOT NULL,
+  `short` varchar(255) DEFAULT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- where-am-i.go
+++ where-am-i.go
@@ -14,13 +14,17 @@ import (
 	"strconv"
 	"time"
 	//"os"
+	"crypto/md5"
+	"encoding/hex"
 	"k8s.io/kubernetes/pkg/util/rand"
+
 )
 
 var q *qqwry.QQwry
-var indexT, guaT *template.Template
-var stmtIns *sql.Stmt
-var stmtCount *sql.Stmt
+var indexT, guaT, tinyUrlT *template.Template
+var urlInsert, urlUpdate, stmtIns, stmtCount *sql.Stmt
+var ALPHABET string = "23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_";
+var BASE int = len(ALPHABET)
 
 func main() {
 	var port int
@@ -38,6 +42,19 @@ func main() {
 	}
 	defer db.Close()
 
+	urlInsert, err = db.Prepare("INSERT INTO shorturl (long, create_time) VALUES( ?, ? )")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer urlInsert.Close()
+
+
+	urlUpdate, err = db.Prepare("UPDATE shorturl SET short = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer urlUpdate.Close()
+
 	stmtIns, err = db.Prepare("INSERT INTO visitor VALUES( ?, ? )") // ? = ip, access time
 	if err != nil {
 		panic(err.Error())
@@ -54,15 +71,115 @@ func main() {
 
 	indexT, _ = template.ParseFiles("tmpl/head.html", "tmpl/header.html", "tmpl/index.html", "tmpl/footer.html")
 	guaT, _ = template.ParseFiles("tmpl/head.html", "tmpl/header.html", "tmpl/gua.html", "tmpl/footer.html")
+	tinyUrlT, _ = template.ParseFiles("tmpl/head.html", "tmpl/header.html", "tmpl/tinyurl.html", "tmpl/footer.html")
 	//t, _ = template.ParseFiles("tmpl/index.html")
 
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/index", indexHandler)
 	http.HandleFunc("/gua", guaHandler)
+	http.HandleFunc("/tinyurl", tinyurlHandler)
+	http.HandleFunc("/", defaultHandler)
 	var address string = fmt.Sprintf(":%d", port)
 	err = http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal("server start failed.", err)
 	}
+}
+
+func encode(num int64) string {
+	var str []string = make([]string, 0, 6)
+	for num > 0 {
+		str = append(str, string(ALPHABET[num % BASE]))
+		num = num / BASE;
+	}
+	return str
+		//StringBuilder str = new StringBuilder();
+		//while (num > 0) {
+		//	str.insert(0, ALPHABET.charAt(num % BASE));
+		//	num = num / BASE;
+		//}
+		//return str.toString();
+}
+
+func getTinyUrl(url string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(url))
+
+	hex.EncodeToString(hasher.Sum(nil))
+
+	now := time.Now()
+	if res, err := urlInsert.Exec(url, "", now.Format("2006-01-02 15:04:05")); err != nil {
+        id, err := res.LastInsertId()
+        if err != nil {
+            println("Error:", err.Error())
+        } else {
+            println("LastInsertId:", id)
+        }
+
+		var idStr string = encode(id)
+		if _, err := urlUpdate.Exec(idStr, id); err != nil {
+			log.Fatal("update tiny url table failed.", err)
+		}
+
+		return idStr, nil
+	}
+	return "", fmt.Errorf("get failed")
+}
+
+func tinyurlHandler(w http.ResponseWriter, r *http.Request) {
+	var count int
+	var err error
+	var ip string
+	var tmplHash map[string]string = make(map[string]string)
+
+	if ip, _, err = net.SplitHostPort(r.RemoteAddr); err != nil {
+		io.WriteString(w, "internal error: invalid parameter.")
+		return
+	}
+	count, err = insertAndGetVisitorCount(ip)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	tmplHash["VisitorCount"] = strconv.Itoa(count)
+
+	if r.Method == "GET" {
+		indexT.ExecuteTemplate(w, "tinyurl", tmplHash)
+	} else if r.Method == "POST" {
+		r.ParseForm()
+		url := r.Form["url"][0]
+
+		newUrl := getTinyUrl(url)
+
+		var message string = "短地址：" + newUrl
+		tmplHash["Message"] = message
+
+		if nil != indexT.ExecuteTemplate(w, "tinyurl", tmplHash) {
+			io.WriteString(w, "internal error: 502")
+			return
+		}
+	}
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	//indexHandler(w, r)
+	path := r.URL.Path
+	fmt.Println(path)
+	if path == "/" {
+		io.WriteString(w, "hello boy.")
+		return
+	}
+
+	var newPath string = "www.baidu.com"
+	if path == "/abcde" {
+		newPath = "https://www.baidu.com/s?wd=%E5%93%88%E5%93%88%E5%93%88%E5%93%88&rsv_spt=1&rsv_iqid=0x8d3ff876000032ae&issp=1&f=3&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=cn&tn=baiduhome_pg&rsv_enter=1&oq=hhhh&inputT=2997&rsv_t=3dd7RQr4AlMzBIPfHVuNzVIaE8Cc0WWg81enKr2u0sKzRc2DFt%2BoUoOZVKsVOY%2FY4i8d&rsv_sug3=5&rsv_sug1=5&rsv_sug7=100&rsv_pq=b3f2ba48000046e6&rsv_sug2=1&prefixsug=hhhh&rsp=3&rsv_sug4=3384&rsv_sug=1"
+	}
+
+	http.Redirect(w, r, newPath, http.StatusFound)
+
+	//w.WriteHeader(302)
+	//w.Header().Set("Location", newPath)
+
+	return
 }
 
 var yiJing []string = []string{"或跃在渊．无咎",
@@ -148,7 +265,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		tmplHash["Message"] = message
 
 		if nil != indexT.ExecuteTemplate(w, "index", tmplHash) {
-		//if nil != indexT.Execute(w, tmplHash) {
+			//if nil != indexT.Execute(w, tmplHash) {
 			io.WriteString(w, "internal error: 502")
 			return
 		}
