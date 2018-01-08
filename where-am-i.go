877f4a03597db2f762e428be1a71fb040c63f343
silenceshell <me@ieevee.com>
2018-01-09 00:18:22 +0800

    update

--- where-am-i.go
+++ where-am-i.go
@@ -117,10 +117,10 @@ func getMacInfo(macAddress string) (*macInfo, error) {
 	macInfo.mac = macAddress
 
 	resp, err := http.Get(fmt.Sprintf("https://macvendors.co/api/%s/", macAddress))
-	defer resp.Body.Close()
 	if err!= nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
 
 	//result := []byte(`{"result":{"company":"Apple, Inc.","mac_prefix":"08:74:02","address":"1 Infinite Loop,Cupertino  CA  95014,US","start_hex":"087402000000","end_hex":"087402FFFFFF","country":"US","type":"MA-L"}}`)
 	result, err := ioutil.ReadAll(resp.Body)
